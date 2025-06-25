cd8acc00a57fe899ee5c1b7a1647fa99147bbc31
rikoeldon <106416799+rikoeldon@users.noreply.github.com>
2025-06-24 21:24:30 -0400

    added mock cli parser using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod cli_mock_draft;
+use crate::cli_mock_draft::parse_tx_id;
+use clap::Parser;
+use cli_mock_draft::Cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli_args = Cli::parse();
+    parse_tx_id(&cli_args.tx_id, cli_args.out.as_deref());
 }
